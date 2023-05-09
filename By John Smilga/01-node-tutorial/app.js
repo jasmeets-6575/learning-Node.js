50f66f4fd896a6c2814d29daa1d111be77c9bc69
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-10 04:06:01 +0530

    node.js async

--- By John Smilga/01-node-tutorial/app.js
+++ By John Smilga/01-node-tutorial/app.js
@@ -1,19 +1,12 @@
-const http = require("http");
+const { readFile } = require("fs");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("welcome to our home page");
+console.log("start");
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
     return;
   }
-  if (req.url === "/about") {
-    res.end("Here is our short history");
-    return;
-  }
-  res.end(`
-  <h1>Oops!</h1>
-  <p>We cant seem to find the page you are looking for...</p>
-  <a href="/">back home</a>
-  `);
-});
-
-server.listen(5000);
+  console.log(result);
+  console.log("completed first task");
+})
+console.log("starting next task");
