7c3870336fd970ea3162448953a7d0997e3d9f34
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-13 02:08:06 +0530

    node.js express JSON example

--- By John Smilga/02-express-tutorial/app.js
+++ By John Smilga/02-express-tutorial/app.js
@@ -3,9 +3,16 @@ const {products} = require("./data")
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json(products);
-});
+  res.send(`<h1>Home Page</h1> <a href="/api/products">products</a>`);
+}); 
 
+app.get("/api/products",(req,res)=>{
+  const newProducts = products.map((product)=>{
+    const {id,name,image} = product;
+    return { id,name,image}
+  })
+  res.json(newProducts)
+})
 app.all("*", (req, res) => {
   res.status(404).send("resource not found");
 });
