65d77a9bd2186afe171ee8ab224a17e0313c03a8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-13 03:23:36 +0530

    node.js express app example

--- By John Smilga/02-express-tutorial/app.js
+++ By John Smilga/02-express-tutorial/app.js
@@ -17,13 +17,35 @@ app.get("/api/products", (req, res) => {
 app.get("/api/products/:productID", (req, res) => {
   // console.log(req);
   // console.log(req.params);
-  const {productID} = req.params;
+  const { productID } = req.params;
 
-  const singleProduct = products.find((product) => product.id === Number(productID));
-  if(!singleProduct){
-    return res.status(404).send("Product does not exist ")
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist ");
   }
   res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(400).send("Product does not exist");
+    return res.status(200).json({success: true, data: []})
+  }
+  res.status(200).json(sortedProducts);
+  
 });
 
 app.all("*", (req, res) => {
