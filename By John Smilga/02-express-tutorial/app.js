595d6946a157e6258048446103eab609ff96f393
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-13 20:44:14 +0530

    node.js express middleware

--- By John Smilga/02-express-tutorial/app.js
+++ By John Smilga/02-express-tutorial/app.js
@@ -1,54 +1,19 @@
 const express = require("express");
-const { products } = require("./data");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1> <a href="/api/products">products</a>`);
+const logger = (re, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+app.get("/", logger, (req, res) => {
+  res.send(`<h1>Home Page</h1>`);
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  const { productID } = req.params;
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist ");
-  }
-  res.json(singleProduct);
-});
-
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(400).send("Product does not exist");
-    return res.status(200).json({ success: true, data: [] });
-  }
-  res.status(200).json(sortedProducts);
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/about", logger, (req, res) => {
+  res.send(`<h1>About Page</h1>`);
 });
 
 app.listen(5000, () => {
