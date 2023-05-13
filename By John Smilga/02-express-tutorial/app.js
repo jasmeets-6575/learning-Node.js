1ab6724f28990a35a637984a0ca6a96ad5cb589e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-14 00:02:36 +0530

    node.js express get method

--- By John Smilga/02-express-tutorial/app.js
+++ By John Smilga/02-express-tutorial/app.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
+let { people } = require("./data");
 
-app.use([logger, authorize]);
-app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1>`);
+app.get("/api/products", (req, res) => {
+  res.status(400).json({  success:true , data: people });
 });
-
-app.get("/about", (req, res) => {
-  res.send(`<h1>About Page</h1>`);
-});
-
 app.listen(5000, () => {
   console.log("server is listening on port 5000..");
 });
