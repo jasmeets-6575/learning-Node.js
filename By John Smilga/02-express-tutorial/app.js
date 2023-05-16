5c7c00c45943137372fa43b2a497d36d0f699154
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-17 00:12:29 +0530

    node.js express delete method postman

--- By John Smilga/02-express-tutorial/app.js
+++ By John Smilga/02-express-tutorial/app.js
@@ -44,16 +44,33 @@ app.post("./login", (req, res) => {
 app.put("/api/people/:id", (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
-  const person = people.find((person) => {
-    person.id === Number(id);
-  });
+
+  const person = people.find((person) => person.id === Number(id));
   if (!person) {
     return res
-      .status(400)
-      .json({ success: false, msg: " person is not there" });
+      .status(404)
+      .json({ success: false, msg: ` no person with id ${id}` });
   }
-
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
   res.status(201).json({ success: true, data: newPeople });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
