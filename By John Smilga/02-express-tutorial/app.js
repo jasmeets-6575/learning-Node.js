b413ebfbd7b0d8326332d10807406694856173fe
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-20 23:01:10 +0530

    node.js implementing express.js setup

--- By John Smilga/02-express-tutorial/app.js
+++ By John Smilga/02-express-tutorial/app.js
@@ -1,22 +1,19 @@
-const express = require("express");
-const app = express();
-let { people } = require("./data");
+const express = require('express')
+const app = express()
+
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
 // static assets
-app.use(express.static("./methods-public"));
+app.use(express.static('./methods-public'))
 // parse form data
-app.use(express.urlencoded({ extended: false }));
-// parse json data
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
-app.post("./login", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-  res.status(401).send("Please provide data");
-});
+app.use('/api/people', people)
+app.use('/login', auth)
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000..");
-});
+  console.log('Server is listening on port 5000....')
+})
