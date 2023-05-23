529ccaae383a9369693c39ccb306082b413ea01f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-05-23 17:36:51 +0530

    task manager api

--- By John Smilga/03-task-manager/starter/app.js
+++ By John Smilga/03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+//middleware
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+app.use("/api/v1/tasks", tasks);
+app.use("/api/v1/tasks/:id", tasks);
+app.use("/api/v1/tasks/:id", tasks);
+
+const port = 3000;
+app.listen(port, console.log(`server is listening on ${port}`));
---
+++ By John Smilga/03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,22 @@
+const getAllTasks = (req, res) => {
+  res.send("all items");
+};
+const createTasks = (req, res) => {
+  res.send("create task");
+};
+const getTask = (req, res) => {
+  res.send("create task");
+};
+const updateTask = (req, res) => {
+  res.send("create task");
+};
+const deleteTask = (req, res) => {
+  res.send("create task");
+};
+
+module.exports = { 
+    getAllTasks, 
+    createTasks, 
+    getTask, 
+    updateTask, 
+    deleteTask };
