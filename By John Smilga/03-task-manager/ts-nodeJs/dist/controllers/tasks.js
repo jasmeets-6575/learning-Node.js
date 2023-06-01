3880e9b0296e7057aea07e1189f47a5bd92612b0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-01 20:49:14 +0530

    task manager adding get tasks

--- By John Smilga/03-task-manager/ts-nodeJs/dist/controllers/tasks.js
+++ By John Smilga/03-task-manager/ts-nodeJs/dist/controllers/tasks.js
@@ -14,13 +14,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteTask = exports.updateTask = exports.getTask = exports.createTasks = exports.getAllTasks = void 0;
 const Task_1 = __importDefault(require("../models/Task"));
-const getAllTasks = (req, res) => {
-    res.send("get all tasks");
-};
+const getAllTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const tasks = yield Task_1.default.find({});
+        res.status(200).json({ tasks });
+    }
+    catch (error) {
+        res.status(500).json({ msg: error });
+    }
+});
 exports.getAllTasks = getAllTasks;
 const createTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const task = yield Task_1.default.create(req.body);
-    res.status(201).json({ task });
+    try {
+        const task = yield Task_1.default.create(req.body);
+        res.status(201).json({ task });
+    }
+    catch (error) {
+        res.status(500).json({ msg: error });
+    }
 });
 exports.createTasks = createTasks;
 const getTask = (req, res) => {
--- By John Smilga/03-task-manager/ts-nodeJs/src/controllers/tasks.ts
+++ By John Smilga/03-task-manager/ts-nodeJs/src/controllers/tasks.ts
@@ -1,13 +1,22 @@
 import { RequestHandler } from "express";
 import Task from "../models/Task";
 
-export const getAllTasks: RequestHandler = (req, res) => {
-  res.send("get all tasks");
+export const getAllTasks: RequestHandler = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 export const createTasks: RequestHandler = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 export const getTask: RequestHandler = (req, res) => {
