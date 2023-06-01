d02e52d386e4dad02c6fa415f25d4e524ea4d4c9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-01 19:19:53 +0530

    task manager adding validation

--- By John Smilga/03-task-manager/ts-nodeJs/dist/models/Task.js
+++ By John Smilga/03-task-manager/ts-nodeJs/dist/models/Task.js
@@ -4,5 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const TaskSchema = new mongoose_1.default.Schema({ name: String, completed: Boolean });
+const TaskSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: [true, "must provide name"],
+        trim: true,
+        maxlength: [20, "name can not be more than 20 characters"],
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+});
 exports.default = mongoose_1.default.model("Task", TaskSchema);
--- By John Smilga/03-task-manager/ts-nodeJs/src/models/Task.ts
+++ By John Smilga/03-task-manager/ts-nodeJs/src/models/Task.ts
@@ -1,10 +1,21 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-interface SchemaInput extends Document{
+interface SchemaInput extends Document {
   name: string;
   completed: Boolean;
 }
 
-const TaskSchema:Schema = new mongoose.Schema({ name: String, completed: Boolean });
+const TaskSchema: Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name can not be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-export default mongoose.model<SchemaInput>("Task", TaskSchema)
+export default mongoose.model<SchemaInput>("Task", TaskSchema);
