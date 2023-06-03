f8b598dea0ceeff6e17692a3342499ed2563139b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-03 21:27:39 +0530

    adding query param

--- By John Smilga/04-store-api/ts-my-app/src/controllers/product.ts
+++ By John Smilga/04-store-api/ts-my-app/src/controllers/product.ts
@@ -1,5 +1,14 @@
-import { RequestHandler } from "express";
+import { RequestHandler, query } from "express";
 import Product from "../models/product";
+
+interface queryParameter {
+    name?: string;
+    price?: number;
+    featured?: boolean;
+    rating?: number;
+    createdAt?: Date;
+    company?: string;
+  }
 
 export const getAllProductsStatic: RequestHandler = async (req, res) => {
   const products = await Product.find({
@@ -9,6 +18,11 @@ export const getAllProductsStatic: RequestHandler = async (req, res) => {
 };
 
 export const getAllProducts: RequestHandler = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+  const queryObject:queryParameter = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await Product.find(queryObject);
   res.status(200).json({ nbHits: products.length, products });
 };
