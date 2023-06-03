ed639656280bbf62ccfeb80a6cd02df74256fbcc
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-03 23:47:42 +0530

    adding sort

--- By John Smilga/04-store-api/ts-my-app/src/controllers/product.ts
+++ By John Smilga/04-store-api/ts-my-app/src/controllers/product.ts
@@ -2,13 +2,13 @@ import { RequestHandler, query } from "express";
 import Product from "../models/product";
 
 interface queryParameter {
-    name?: string;
-    price?: number;
-    featured?: boolean;
-    rating?: number;
-    createdAt?: Date;
-    company?: string;
-  }
+  name?: string;
+  price?: number;
+  featured?: boolean;
+  rating?: number;
+  createdAt?: Date;
+  company?: string;
+}
 
 export const getAllProductsStatic: RequestHandler = async (req, res) => {
   const products = await Product.find({
@@ -18,11 +18,26 @@ export const getAllProductsStatic: RequestHandler = async (req, res) => {
 };
 
 export const getAllProducts: RequestHandler = async (req, res) => {
-  const { featured } = req.query;
-  const queryObject:queryParameter = {};
+  const { featured, company, name, sort } = req.query;
+  const queryObject: queryParameter = {};
+  //   const regexObj = { $regex: name, $options: "i" };
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  const products = await Product.find(queryObject);
+  if (typeof company === "string") {
+    queryObject.company = company;
+  }
+  if (typeof name === "string") {
+    queryObject.name = name;
+  }
+
+  let result = Product.find(queryObject);
+  if (typeof sort === "string") {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
