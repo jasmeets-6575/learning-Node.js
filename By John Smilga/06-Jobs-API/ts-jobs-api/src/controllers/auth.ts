8127d29df076a81ed95755972fa958f90bd7b363
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-04 23:05:04 +0530

    adding error handling

--- By John Smilga/06-Jobs-API/ts-jobs-api/src/controllers/auth.ts
+++ By John Smilga/06-Jobs-API/ts-jobs-api/src/controllers/auth.ts
@@ -1,8 +1,17 @@
 import { RequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
+import User from "../models/User";
+import { BadRequest } from "../errors/bad-request";
 
 export const register: RequestHandler = async (req, res) => {
-  res.send("register use");
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequest("Please provide all inputs");
+  }
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
+
 export const login: RequestHandler = async (req, res) => {
-  res.send("login use");
+  res.send("login user");
 };
