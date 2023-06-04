02a057c2fe57c17aa66168c54e6d22c853edfa90
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-04 20:39:17 +0530

    a

--- By John Smilga/05-JWT-basics/ts-jsw/src/middleware/error-handler.ts
+++ By John Smilga/05-JWT-basics/ts-jsw/src/middleware/error-handler.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response, Request } from "express";
 import { CustomAPIError } from "../errors/custom-error";
-
+import { StatusCodes } from "http-status-codes";
 
 export const errorHandlerMiddleware = async (
   err: Error,
@@ -9,7 +9,7 @@ export const errorHandlerMiddleware = async (
   next: NextFunction
 ) => {
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message });
   }
   return res.status(500).send("Something went wrong try again later");
 };
