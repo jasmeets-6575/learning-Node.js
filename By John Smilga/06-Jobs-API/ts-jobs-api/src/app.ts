555483416bf755234ad787c44e05eb86b2bf7c3b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-04 22:39:41 +0530

    adding model

--- By John Smilga/06-Jobs-API/ts-jobs-api/src/app.ts
+++ By John Smilga/06-Jobs-API/ts-jobs-api/src/app.ts
@@ -5,9 +5,11 @@ import { errorHandlerMiddleware } from "./middleware/error-handler";
 import { json } from "body-parser";
 import authRouter from "./routes/auth"
 import jobsRouter from "./routes/jobs"
+import { connectDB } from "./db/connect";
 
 dotenv.config();
 const app = express();
+
 
 // middleware
 app.use(json());
@@ -20,11 +22,15 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
+const mongoURI:string | undefined = process.env.MONGO_URI
 
 const start = async () => {
   try {
-    // connectDB
+   if(mongoURI){
+    await connectDB(mongoURI)
     app.listen(port, () => console.log(`Server is listening port ${port}`));
+   }
+    // connectDB
   } catch (error) {
     console.log(error);
   }
---
+++ By John Smilga/06-Jobs-API/ts-jobs-api/src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide name"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+  },
+});
+
+export default mongoose.model("User", UserSchema)
