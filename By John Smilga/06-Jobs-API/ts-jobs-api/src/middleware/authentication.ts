3312926a126746c0ba4c026af9330164c255a388
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-05 03:31:32 +0530

    adding authentication middleware

---
+++ By John Smilga/06-Jobs-API/ts-jobs-api/src/middleware/authentication.ts
@@ -0,0 +1,30 @@
+import { NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Response } from "express";
+import { unAuthenticated } from "../errors/unauthenticated";
+
+const JWT_URI: string | undefined = process.env.JWT_SECRET;
+
+export const authMiddleware = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new unAuthenticated("No token provided");
+  }
+
+  const token: string = authHeader.split(" ")[1];
+  try {
+    if (JWT_URI) {
+      const payload = jwt.verify(token, JWT_URI) as JwtPayload;
+
+      req.user = { userId: payload.userId, name: payload.name };
+      next();
+    }
+  } catch (error) {
+    throw new unAuthenticated("Not authorized to access this route");
+  }
+};
